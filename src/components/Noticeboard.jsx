39a88c56191cd1b042599433eb7dd58add154f1e
MHThe1 <mehedihtanvir@gmail.com>
2025-12-10 19:31:20 +0600

    updated

--- src/components/Noticeboard.jsx
+++ src/components/Noticeboard.jsx
@@ -15,7 +15,7 @@ export default function Noticeboard() {
             </h3>
             <div className="mt-1">
               <p className="text-gray-700 dark:text-gray-300 animate-pulse">
-                Spring 2025 data is being updated. Please stand by! 
+                Spring 2026 data is being updated. Please stand by! 
                 <br />
               </p>
             </div>
